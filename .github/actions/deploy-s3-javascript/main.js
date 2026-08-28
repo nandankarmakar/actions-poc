dea7d17a8bbffbf1336847280f8864f0daa843ca
Nandan <nandan.karmakar739@gmail.com>
2026-08-29 00:13:38 +0530

    Added build Action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,18 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
 
 function run(){
+
+    // 1) Get Some input Info
+    const bucket = core.getInput('bucket', {required: true})
+    const bucketRegion = core.getInput('bucket-region', {required: true})
+    const distFolder = core.getInput('dist-folder', {required:true})
+
+    //2) Upload Bucket
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
     core.notice('Hello from my custom javascript action')
 }
 
